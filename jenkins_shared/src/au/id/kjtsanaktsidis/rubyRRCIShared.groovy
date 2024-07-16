71eea6c08f19c92bdf340974ac993219daf587ad
KJ Tsanaktsidis <kj@kjtsanaktsidis.id.au>
2024-07-16 21:29:21 +1000

    Apparently it must be src

---
+++ jenkins_shared/src/au/id/kjtsanaktsidis/rubyRRCIShared.groovy
@@ -0,0 +1,8 @@
+class rubyRRCIShared {
+    static def latestVersion() {
+      Jenkins.instance.getItemByFullName('ruby-rr-ci Tester Image/main')
+                      .getLastSuccessfulBuild()
+                      .getAction(hudson.plugins.git.util.BuildData.class)
+                      .lastBuiltRevision.sha1String
+    }
+}
--- jenkins_shared/vars/rubyRRCIShared.groovy
+++
@@ -1,7 +0,0 @@
-def latestVersion() {
-  Jenkins.instance.getItemByFullName('ruby-rr-ci Tester Image/main')
-                  .getLastSuccessfulBuild()
-                  .getAction(hudson.plugins.git.util.BuildData.class)
-                  .lastBuiltRevision.sha1String
-}
-
